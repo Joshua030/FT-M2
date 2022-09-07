49d31693354010fb038a72e62f2d7f0d524a4c77
Arq. Carolina Arias <joseluy123@gmail.com>
2022-09-06 22:17:22 -0500

    second commit

--- 01-DOM/homework/DOMhomework.js
+++ 01-DOM/homework/DOMhomework.js
@@ -1,13 +1,13 @@
 // Crear un array vacío llamado 'toDoItems'
 // Tu codigo acá:
-
+let toDoItems= [];
 
 // En la página 'index.html' hay un elemento span cuyo texto es 'Aplicación creada por:'.
 // Usando querySelector seleccionar dicho span por su id ('createdBy') y luego usando innerHTML
 // agregar tu nombre al final del texto actual. Ej: 'Aplicación creada por Franco'
 // Tu código acá:
-
-
+const created = document.querySelector("#createdBy");
+created.innerHTML += " Jose";
 
 // Crear una clase denominada 'ToDo' cuyo constructor debe recibir un único parámetro del tipo string
 // con el nombre 'description' que será justamente la descripción del ToDo.
@@ -16,9 +16,13 @@
 // 2) 'complete'    : debe setearse en false
 // Ayuda: usar 'this' en el constructor
 
-function ToDo () {
+function ToDo (description) {
   // Tu código acá:
-
+  if (typeof(description) !== "string") {
+    throw new IllegalArgumentException("invalid input. It should be a string" );
+}
+this.description = description ;
+this.complete = false ;
 }
 
 
@@ -27,7 +31,9 @@ function ToDo () {
 // Debe setear el atributo 'complete' del ToDo en true
 
 // Tu código acá:
-
+ToDo.prototype.completeToDo = function(){
+  return this.complete = true;
+}
 
 
 // Agregar dos parámetros a la función 'buildToDo':
@@ -49,7 +55,17 @@ function ToDo () {
 
 
 function buildToDo(todo, index) {
+ 
   // Tu código acá:
+let toDoShell = document.createElement("div");
+toDoShell.classList.add("toDoShell");
+let toDoText = document.createElement("span");
+document.querySelector("span").addEventListener("click",completeToDo);
+toDoText.innerHTML = todo["description"];
+toDoText.setAttribute("id", index);
+if(todo["complete"]) toDoText.classList.add('completeText');
+toDoShell.appendChild(toDoText);
+return toDoShell;
 
 }
 
@@ -60,7 +76,7 @@ function buildToDo(todo, index) {
 
 function buildToDos(toDos) {
   // Tu código acá:
-
+return toDos.map(buildToDo) ;
 }
 
 
@@ -75,6 +91,12 @@ function buildToDos(toDos) {
 
 function displayToDos() {
   // Tu código acá:
+ let toDoContainer= document.querySelector("#toDoContainer");
+ toDoContainer.innerHTML= "";
+ let newArray = buildToDos(toDoItems);
+ for (let value of newArray) {
+  toDoContainer.innerHTML += value;
+}
 
 }
 
@@ -90,7 +112,11 @@ function displayToDos() {
 
 function addToDo() {
   // Tu código acá:
-
+  let valInput = document.querySelector("#toDoInput").value;
+  let newToDo = new ToDo(valInput);
+  toDoItems.push(newToDo);
+  valInput = "";
+  displayToDos();
 }
 
 // Agregar un 'Event Listener' para que cada vez que el botón 'AGREGAR' sea clickeado
@@ -99,7 +125,8 @@ function addToDo() {
 //   2) Agregarle un 'click' event listener, pasándole la función 'addToDo' como callback
 
 // Tu código acá:
-
+const button1= document.querySelector("#addButton");
+button1.addEventListener("click",addToDo);
 
 // La función completeToDo se va a ejecutar cuando queramos completar un todo
 // [NOTA: Algunas cuestiones a tener en cuenta
@@ -115,8 +142,10 @@ function addToDo() {
 
 function completeToDo(event) {
   // DESCOMENTAR LA SIGUIENTE LINEA
-  // const index = event.target.id;
+  const index = event.target.id;
   // Tu código acá:
+  toDoItems[index].completeToDo();
+  displayToDos();
 
 }
 
@@ -137,7 +166,7 @@ function completeToDo(event) {
 
 
 // Acá debes insertar la llamada a 'displayToDos'
-
+displayToDos()
 
 // ---------------------------- NO CAMBIES NADA DE ACÁ PARA ABAJO ----------------------------- //
 if (typeof module !== 'undefined') {
---
+++ 01-DOM/homework/Questions.md
@@ -0,0 +1,6 @@
+*DOM
+DOM es una abreviatura de Document Object Model. En español podríamos traducirlo por Modelo de Objeto de Documento, aunque en DesarrolloWeb.com nos hemos referido al DOM habitualmente con el nombre de jerarquía de objetos del navegador, porque realmente es una estructura jerárquica donde existen varios objetos y unos dependen de otros
+	* DOM element selectors
+    El uso de «selectores» en JavaScript permite encontrar y seleccionar elementos del DOM bien sea para extraer información de cada nodo o para manipularlos de ser necesario.
+	* DOM events
+    La API de Eventos del DOM nos sirve para volver a nuestro JavaScript reactivo ante determinados sucesos en nuestra interfaz web.
